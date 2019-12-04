59349028cc4ef58bc6abd0ad00d393fb0ecbae57
Farhan Wali <faharn.wali@gmail.com>
2019-12-04 20:27:14 +1300

    Replaced bespoke vec_ types with templated VecT

--- source/app.d
+++ source/app.d
@@ -13,12 +13,36 @@ import camera;
 
 void main( string[] args)
 {
+    uint imageWidth = 640;
+    uint imageHeight = 480;
+
 	// Load the SDL 2 library. 
     DerelictSDL2.load();
 
+	SDL_Window* p_sdlWindow = null;
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		writeln("[ERROR] Couldn't initialise SDL2");
+	}
+	else
+	{
+		writeln("Succesfully initialised SDL2");
+	}
 
-	writeln("Loaded the derelict-sdl2 bindings!");
- 
+	p_sdlWindow = SDL_CreateWindow(
+		"D-Harmonic PT",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		imageWidth,
+		imageHeight,
+		SDL_WINDOW_OPENGL
+	);
+
+	if ( p_sdlWindow == null )
+	{
+		writeln("[ERROR] Could not create window %s", SDL_GetError() );
+	}
+
     // Disable the garbage collector
     GC.disable;
 
@@ -30,8 +54,7 @@ void main( string[] args)
                  vec3( 0.0f, 1.0f, 0.0f )  /* up */,
                  vec3( 0.0f, 0.0f, 0.0f ) /* lookAt */ );
 
-    uint imageWidth = 1920;
-    uint imageHeight = 1080;
+
     int tileSize = 64;
     float* pImageBufferData;
     
@@ -61,6 +84,79 @@ void main( string[] args)
             pImageBufferData[ pixelIndex + 2 ] = b;
         }
     }
+
+    // Set up the pixel format color masks for RGB(A) byte arrays.
+    // Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
+    uint rmask, gmask, bmask, amask;
+// // #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+//     int shift = (req_format == STBI_rgb) ? 8 : 0;
+//     rmask = 0xff000000 >> shift;
+//     gmask = 0x00ff0000 >> shift;
+//     bmask = 0x0000ff00 >> shift;
+//     amask = 0x000000ff >> shift;
+// #else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0; //(req_format == STBI_rgb) ? 0 : 0xff000000;
+// #endif
+
+    SDL_Surface* renderSurface = SDL_CreateRGBSurfaceFrom(
+                                    pImageBufferData,
+                                    imageWidth,
+                                    imageHeight,
+                                    24 /* bits per pixel */,
+                                    imageWidth * 3 /* bytes per row */,
+                                    rmask, gmask, bmask, amask );
+
+	if ( renderSurface != null )
+	{
+		writeln("WE HAVE A VALID RENDER SURFACE");
+	}
+	else
+	{
+		writeln("[ERROR] Invalid render surface!");
+	}
+
+    //The surface contained by the window
+    SDL_Surface* gScreenSurface = null;
+
+    //Get window surface
+    gScreenSurface = SDL_GetWindowSurface( p_sdlWindow );
+
+	SDL_BlitSurface( renderSurface, null, gScreenSurface, null );
+	SDL_UpdateWindowSurface( p_sdlWindow );
+
+
+	SDL_Event e;
+    bool quit = false;
+    size_t numProgressions = 0u;
+    while ( !quit )
+    {
+        while ( SDL_PollEvent( &e ) )
+        {
+            if ( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+            // if (e.type == SDL_KEYDOWN){
+            //     quit = true;
+            // }
+            // if (e.type == SDL_MOUSEBUTTONDOWN){
+            //     quit = true;
+            // }
+        }
+
+
+	}
+
+    // Close and destroy the window
+    //
+    SDL_DestroyWindow( p_sdlWindow );
+
+    // Clean up
+    //
+    SDL_Quit();
 
     ImageBuffer_WriteToPng( &renderImage, cast(char*) "render.png" );
 }
--- source/fwmath.d
+++ source/fwmath.d
@@ -1,5 +1,133 @@
 import std.math;
 
+// alias VecT!( uint, 2 ) vec2ui;
+// alias VecT!( uint, 3 ) vec3ui;
+// alias VecT!( uint, 4 ) vec4ui;
+
+alias VecT!( float, 2 ) vec2f;
+alias VecT!( float, 3 ) vec3f;
+alias VecT!( float, 4 ) vec4f;
+
+alias VecT!(double, 2) vec2d;
+alias VecT!(double, 3) vec3d;
+alias VecT!(double, 4) vec4d;
+
+alias vec2f vec2;
+alias vec3f vec3;
+alias vec4f vec4;
+
+struct VecT( Type, int Dim ) //if (( Dim >= 2 ) && Dim ( <= 4 ))
+{
+    static if ( Dim < 2 )
+    {
+        static assert(0, "Dim value is too low");
+    }
+    else static if ( Dim > 4 )
+    {
+        static assert(0, "Dim value is too high");
+    }
+
+    union
+    {
+        Type[ Dim ] data;
+
+        struct
+        {
+            Type x, y;
+            static if ( Dim >= 3 ) Type z;
+            static if ( Dim >= 4 ) Type w;
+        }
+
+        struct
+        {
+            static if ( Dim == 2 )
+            {
+                Type s, t;
+            }
+            else
+            {
+                Type r, g;
+
+                static if ( Dim >= 3 ) Type b;
+                static if ( Dim >= 4 ) Type a;
+            }
+
+        }
+    }
+
+    alias data  this;    // Means object can be indexed like it's an array
+    alias Dim   dim;
+    alias Type  valueType;
+
+    //  Specialise constructor by dimension
+    //
+    static if ( Dim == 2 ) {
+        this( Type x, Type y ) {
+            data[0] = x;
+            data[1] = y;
+        }
+    }
+    else static if ( Dim == 3 ) {
+        this( Type x, Type y, Type z ) {
+            data[0] = x;
+            data[1] = y;
+            data[2] = z;
+        }
+    }
+    else static if ( Dim == 4 ) {
+        this( Type x, Type y, Type z, Type w ) {
+            data[0] = x;
+            data[1] = y;
+            data[2] = z;
+            data[3] = w;
+        }
+    }
+
+    pure const Type
+    magnitude()
+    {
+        static if ( dim == 2 ) return sqrt( x*x + y*y );
+        else static if ( dim == 3 ) return sqrt( x*x + y*y + z*z );
+        else static if ( dim == 4 ) return sqrt( x*x + y*y + z*z + w*w );
+        else static assert(0, "Too many dimensions!");
+    }
+
+    pure const VecT
+    opBinary( string op )( VecT rhs )
+    {
+        static if ( Dim == 2 ) return mixin( "VecT( x"~op~"rhs.x, y"~op~"rhs.y)" );
+        else static if ( Dim == 3 ) return mixin( "VecT( x"~op~"rhs.x, y"~op~"rhs.y, z"~op~"rhs.z)" );
+        else static if ( Dim == 4 ) return mixin( "VecT( x"~op~"rhs.x, y"~op~"rhs.y, z"~op~"rhs.z, w"~op~"rhs.w)" );
+        else static assert( 0, "Not implemented");
+    }
+
+    pure const VecT
+    opBinary( string op )( float f ) 
+    {
+        VecT v;
+
+        v.data[0] = mixin( "data[0]"~op~"f" );
+        v.data[1] = mixin( "data[1]"~op~"f" );
+        static if ( dim >= 3 )  v.data[2] = mixin( "data[2]"~op~"f" );
+        static if ( dim >= 4 )  v.data[3] = mixin( "data[3]"~op~"f" );
+
+        return v;
+    }
+
+    pure const VecT
+    opBinaryRight( string op )( float f )
+    {
+        VecT v;
+
+        v.data[0] = mixin( "data[0]"~op~"f" );
+        v.data[1] = mixin( "data[1]"~op~"f" );
+        static if ( dim >= 3 )  v.data[2] = mixin( "data[2]"~op~"f" );
+        static if ( dim >= 4 )  v.data[3] = mixin( "data[3]"~op~"f" );
+
+        return v;
+    }
+}
+/*
 //
 //  Math primitives
 //
@@ -36,29 +164,23 @@ struct vec3
     }
 
     pure const vec3
-    opMul( float scalar )
+    opBinary( string op )( vec3 rhs )
     {
-        return vec3( x*scalar, y*scalar, z*scalar );
+        return mixin( "vec3( x"~op~"rhs.x, y"~op~"rhs.y, z"~op~"rhs.z)");
     }
 
     pure const vec3
-    opAdd( vec3 v2 )
+    opBinary( string op )( float v )
     {
-        return vec3( x + v2.x, y + v2.y, z + v2.z );
-    }
-
-    pure const vec3 
-    opSub( vec3 v2 )
-    {
-        return vec3( x - v2.x, y - v2.y, z - v2.z );
+        return mixin( "vec3( v"~op~"x, v"~op~"y, z"~op~"z )" );
     }
 
     pure const vec3
-    opDiv( float scalar )
+    opBinaryRight( string op )( float v )
     {
-        float invScalar =  1.0f/scalar;
-        return vec3( x*invScalar, y*invScalar, z*invScalar );
+        return mixin( "vec3( v"~op~"x, v"~op~"y, z"~op~"z )" );
     }
+
 }
 
 struct vec4
@@ -173,7 +295,7 @@ struct vec2
     }
 }
 
-
+*/
 //
 //  Math primitive functions
 //
@@ -201,8 +323,8 @@ v_normalise( in vec3 v )
 
 struct Ray
 {
-    vec3  m_origin  = {};
-    vec3  m_dir     = {};
+    vec3  m_origin;
+    vec3  m_dir;
 }
 
 struct Sphere
