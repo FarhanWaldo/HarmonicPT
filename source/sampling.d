6c9f25ea62d85a64990bb852120eccc97de37aa3
Farhan Wali <faharn.wali@gmail.com>
2020-07-08 22:15:44 +1200

    Updated sampling method signatures to be pure, nogc, nothrow, and safe

--- source/sampling.d
+++ source/sampling.d
@@ -6,8 +6,8 @@ import fwmath;
     Sampling and PDF methods for basic shapes
 */
 
-pure vec2
-ConcentricSampleDisk( in ref vec2 P )
+pure @nogc @safe nothrow
+vec2 ConcentricSampleDisk( in ref vec2 P )
 {
     vec2 newSample;
 
@@ -30,8 +30,8 @@ ConcentricSampleDisk( in ref vec2 P )
     return newSample;
 }
 
-pure vec3
-UniformSampleHemisphere( in ref vec2 u )
+pure @nogc @safe nothrow
+vec3 UniformSampleHemisphere( in ref vec2 u )
 {
     vec3 sample;
     float z = u.x;
@@ -41,16 +41,16 @@ UniformSampleHemisphere( in ref vec2 u )
     return sample;
 }
 
-pure vec3
-CosineSampleHemisphere( in ref vec2 u )
+pure @nogc @safe nothrow
+vec3 CosineSampleHemisphere( in ref vec2 u )
 {
     vec2 p  = ConcentricSampleDisk( u );
     float z = sqrt( Max( 0.0f, 1.0f - v_dot( p, p ) ) );
     return vec3( p.x, p.y, z );
 }
 
-pure vec3
-UniformSampleSphere( in ref vec2 u )
+pure @nogc @safe nothrow
+vec3 UniformSampleSphere( in ref vec2 u )
 {
     float z     = 1.0f - 2.0f*u.y;
     float r     = sqrt( Max( 0.0f, 1.0f - z*z ) );
@@ -59,8 +59,8 @@ UniformSampleSphere( in ref vec2 u )
     return vec3( r * cos( phi ), r * sin( phi ), z );
 }
 
-pure vec3
-UniformSampleCone( in ref vec2 u, float cosThetaMax )
+pure @nogc @safe nothrow
+vec3 UniformSampleCone( in ref vec2 u, float cosThetaMax )
 {
     float cosTheta =  ( 1.0f - u.x ) + ( u.x * cosThetaMax );
     float sinTheta = sqrt( 1.0f - cosTheta*cosTheta );
@@ -70,20 +70,20 @@ UniformSampleCone( in ref vec2 u, float cosThetaMax )
 }
 
 
-pure float
-UniformHemispherePDF()
+pure @nogc @safe nothrow
+float UniformHemispherePDF()
 {
     return INV_TAU;
 }
 
-pure float
-UniformSpherePDF()
+pure @nogc @safe nothrow
+float UniformSpherePDF()
 {
     return 0.25f * INV_PI;
 }
 
-pure float
-UniformConePdf( float cosThetaMax )
+pure @nogc @safe nothrow
+float UniformConePdf( float cosThetaMax )
 {
     return 1.0f / ( 2.0f * PI * ( 1.0f - cosThetaMax ) );
 }
