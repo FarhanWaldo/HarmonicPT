79d465fd3fbfec8419ed073ec65b06b83e5bf285
Farhan Wali <faharn.wali@gmail.com>
2020-07-08 22:18:28 +1200

    Added new methods to uniformly sample sphere shape, and sample sphere from a ref point by uniformly sampling subtended cone from ref point to sphere

--- source/shape.d
+++ source/shape.d
@@ -1,6 +1,7 @@
 import std.math : sqrt;
 import fwmath;
 import interactions;
+import sampling;
 
 enum EShape 
 {
@@ -8,6 +9,9 @@ enum EShape
     Sphere,
 	Triangle,
 }
+
+alias const(ShapeCommon)     CShapeCommon;
+alias const(ShapeSphere)     CShapeSphere;
 
 struct ShapeCommon
 {
@@ -116,4 +120,121 @@ bool Sphere_IntersectsRay( const(ShapeSphere)* shpSphere, const(Ray)* ray, ref I
 
         return intersects;
 
+}
+
+pragma(inline, true) pure @nogc @trusted nothrow
+Interaction
+Shape_Sample( CShapeCommon* shape, CInteraction* refPoint, in vec2 randomSample )
+{
+    switch ( shape.m_shapeType )
+	{
+	    case EShape.Sphere:
+        auto sphere = cast( CShapeSphere* ) shape;
+		return Sphere_Sample( sphere, refPoint, randomSample );
+
+		default:
+		assert("Unimplemented shape type");
+		return Interaction();
+	}
+}
+
+pure @nogc @safe nothrow
+Interaction
+Sphere_Sample( CShapeSphere* sphere, in vec2 randomSample )
+{
+    vec3 n = UniformSampleSphere( randomSample );
+	vec3 p = sphere.m_geo.m_centre + sphere.m_geo.m_radius*n;
+
+    auto  intx = Interaction( p, n, 0.0f /* FW_TODO::[time] */ );
+	intx.m_posError = 0.00001 * p.abs(); /* FW_TODO::[precision] */ 
+	
+	return intx;
+}
+
+pure @nogc @safe nothrow
+Interaction
+Sphere_Sample( CShapeSphere* sphere, CInteraction* refPoint, in vec2 randomSample )
+{
+    /// F_TODO:: Implement this sampling function, then DiffuseAreaLight_SampleIrradiance
+	///
+    const vec3  centre = sphere.m_geo.m_centre;
+    const float r      = sphere.m_geo.m_radius;
+	const float r2     = r*r; // radius rsquared
+    const vec3  refPos = refPoint.m_pos;
+	const vec3  refPointToCentre = centre - refPos;
+
+	const float distToRefSquared = refPointToCentre.magnitudeSquared();
+	// Fallback to random, but uniform sampling of the unit sphere if ref point is inside circle
+	//
+	if ( distToRefSquared < r2 )
+	{
+	    return Sphere_Sample( sphere, randomSample );
+	}
+
+
+	pure @nogc @safe nothrow void
+	CreateOnb( in vec3 e0, ref vec3 e1, ref vec3 e2 )
+	{
+	     if ( Abs( e0.x ) > Abs( e0.y ) ) {
+		     e1 = v_normalise( vec3( -e0.z, 0.0f, e0.x ) ); 
+		 }
+		 else {
+		     e1 = v_normalise( vec3( 0.0f, e0.z, -e0.y ) );  
+		 }
+
+		 e2 = v_cross( e0, e1 );
+	}
+	// Create a coordinate system where the z axis faces from the ref point
+	//   to the centre of the sphere
+	//
+	const vec3 _z = v_normalise( refPointToCentre );
+	vec3 _y, _x;
+	CreateOnb( _z, _x, _y );
+
+    const vec2 u = randomSample;
+
+	//  Find sin and cos of theta for the right-angle triangle formed from the centre of the sphere
+	//   with a line joining the centre and ref point, and the centre to a point on the radius of the sphere,
+	//      perpendicular to the other line
+	//
+	//  This gives us the max range of theta for the cone subtended from the refPoint to the sphere
+	//
+	const float sinThetaMax2 = r*r/distToRefSquared;
+	const float cosThetaMax  = sqrt( Max( 0.0f, 1.0f - sinThetaMax2 ) );
+	const float cosTheta     = (1.0f - u.x) + u.x*cosThetaMax;
+	const float sinTheta     = sqrt( Max( 0.0f, 1.0f - cosTheta*cosTheta ) );
+	const float phi          = u.y * TAU;
+
+	const float distToCentre = sqrt( distToRefSquared ); // distToRef and disToCentre are interchangeable
+	const float distToSurface =
+	    distToCentre*cosTheta - sqrt( Max(0.0f, r2 - distToCentre*distToCentre*sinTheta*sinTheta) );
+		
+    const float cosAlpha =
+	    ( distToCentre*distToCentre + r2 - distToSurface*distToSurface ) /
+		( 2.0f*distToCentre*r );
+    const float sinAlpha = sqrt( Max( 0.0f, 1.0f - cosAlpha*cosAlpha ) );
+
+	import std.math : sin, cos;
+	//  Use above basis vectors to determine world space normals s
+    //  
+    //  Note::  { _x, _y, _z } is a orthonormal basis centred around the ref point
+    //          { -_x, -_y, -_z } is an equivalent basis centred around a point lying on the sphere
+    //
+	//
+	const vec3 sphereN =
+	    cosTheta*sin(phi)*(-1.0f*_x) +
+		sinTheta*sin(phi)*(-1.0f*_y) +
+		cos(phi)         *(-1.0f*_z);
+
+    vec3 sphereP = r * sphereN;
+	sphereP *= r/(sphereP.magnitude()); // apply some numerical correction...
+
+	const vec3 pError = 0.0001f * sphereP.abs(); /* FW_TODO::[precision] */
+
+    const vec3 offsetSphereP = sphereP + centre + 0.001*sphereN;
+	
+    auto intx = Interaction( offsetSphereP , sphereN, 0.0f /* FW_TODO::[time] */ );
+	intx.m_posError = pError;
+	
+	return intx;
 }
