e3d34f12d071286285a6da4f12cab80dbf0b6b78
Farhan Wali <faharn.wali@gmail.com>
2020-08-01 00:14:47 +1200

    moved Bsdf into bsdf.d, using BufferT for lobe stack, made all the methods in Bxdf pure safe nogc and nothrow

--- source/bsdf.d
+++ source/bsdf.d
@@ -1,7 +1,69 @@
 import fwmath;
 import bxdf;
+import datastructures;
 
-class Bsdf
+struct Bsdf
 {
-    
+    vec3  m_geoNormal;
+
+	//  Can be used to transform in/out of the shading coordinate system
+	//  { m_shadingNormal, m_shadingS, m_shadingT } form an orthonormal basis on the surface of the shading point
+	//
+	vec3  m_shadingNormal;
+	vec3  m_shadingS;
+	vec3  m_shadingT;
+
+	// BaseBxDF*[8]  m_bxdfs;
+    // uint          m_numBxdfs = 0;
+	enum MaxLobes = 8;
+	alias BufferT!( BaseBxDF*, MaxLobes)  BxDFStack;
+    BxDFStack m_bxdfs;
+	
+	float         m_eta; // Relative index of refraction
+
+	pure @safe @nogc nothrow
+	this( in vec3 geoNormal, in vec3 shadingNormal, in vec3 dpdu, float eta = 1.0f )
+	{
+	    m_geoNormal     = geoNormal;
+		m_shadingNormal = shadingNormal;
+		m_shadingS      = v_normalise( dpdu );
+		m_shadingT      = v_normalise( v_cross( shadingNormal, dpdu ) );
+		m_eta           = eta;
+	}
+
+	pure @safe @nogc nothrow
+	void AddBxDF( BaseBxDF* bxdf ) {
+		m_bxdfs.Push( bxdf );
+	}
+
+	pure const @safe @nogc nothrow
+	uint NumComponents( BxDFType flags = BxDFType.All )
+	{
+	    uint numMatching = 0;
+		foreach( bxdf; m_bxdfs.range() ) {
+			if ( bxdf.MatchesType( flags ) ) {
+				++numMatching;
+			}
+		}
+		return numMatching;
+	}
+
+	pure const @safe @nogc nothrow
+	vec3 WorldToLocal( in vec3 w )
+	{
+	    return vec3(
+		    v_dot( w, m_shadingS ),
+			v_dot( w, m_shadingT ),
+			v_dot( w, m_shadingNormal ) );
+	}
+
+	pure const @safe @nogc nothrow
+	vec3 LocalToWorld( in vec3 w )
+	{
+	    return vec3(
+		    m_shadingS.x*w.x + m_shadingT.x*w.y + m_shadingNormal.x*w.z,
+		    m_shadingS.y*w.x + m_shadingT.y*w.y + m_shadingNormal.y*w.z,
+		    m_shadingS.z*w.x + m_shadingT.z*w.y + m_shadingNormal.z*w.z,
+		);
+	}
 }
--- source/bxdf.d
+++ source/bxdf.d
@@ -42,47 +42,53 @@ enum BxDFType
 //
 */
 
-pure float CosTheta( in vec3 w )        { return w.z; }
-pure float Cos2Theta( in vec3 w )       { return w.z * w.z; }
-pure float AbsCosTheta( in vec3 w )     { return Abs( w.z ); }
+pure @safe @nogc nothrow float CosTheta( in vec3 w )        { return w.z; }
+pure @safe @nogc nothrow float Cos2Theta( in vec3 w )       { return w.z * w.z; }
+pure @safe @nogc nothrow float AbsCosTheta( in vec3 w )     { return Abs( w.z ); }
 
-pure float Sin2Theta( in vec3 w )       { return Max( 0.0, 1.0 - Cos2Theta( w ) ); }
-pure float SinTheta( in vec3 w )        { return sqrt( Sin2Theta( w ) ); }
-pure float TanTheta( in vec3 w )        { return SinTheta( w ) / CosTheta( w ); }
-pure float Tan2Theta( in vec3 w )       { return Sin2Theta( w ) / Cos2Theta( w ); }
+pure @safe @nogc nothrow float Sin2Theta( in vec3 w )       { return Max( 0.0, 1.0 - Cos2Theta( w ) ); }
+pure @safe @nogc nothrow float SinTheta( in vec3 w )        { return sqrt( Sin2Theta( w ) ); }
+pure @safe @nogc nothrow float TanTheta( in vec3 w )        { return SinTheta( w ) / CosTheta( w ); }
+pure @safe @nogc nothrow float Tan2Theta( in vec3 w )       { return Sin2Theta( w ) / Cos2Theta( w ); }
 
-pure float CosPhi( in vec3 w ) {
+pure @safe @nogc nothrow
+float CosPhi( in vec3 w ) {
     float sinTheta = SinTheta( w );
 	return ( sinTheta == 0.0f ) ? 1.0f : Clamp( w.x / sinTheta, -1.0f, 1.0f );
 }
 
-pure float SinPhi( in vec3 w ) {
+pure @safe @nogc nothrow
+float SinPhi( in vec3 w ) {
     float sinTheta = SinTheta( w );
 	return ( sinTheta == 0.0f ) ? 0.0f : Clamp( w.y / sinTheta, -1.0f, 1.0f );
 }
 
-pure float Cos2Phi( in vec3 w ) {
+pure @safe @nogc nothrow
+float Cos2Phi( in vec3 w ) {
     float cosPhi = CosPhi( w );
 	return cosPhi * cosPhi;
 }
 
-pure float Sin2Phi( in vec3 w ) {
+pure @safe @nogc nothrow
+float Sin2Phi( in vec3 w ) {
     float sinPhi = SinPhi( w );
 	return sinPhi*sinPhi;
 }
 
 
-pure float CosDPhi( in vec3 wa, in vec3 wb ) {
+pure @safe @nogc nothrow
+float CosDPhi( in vec3 wa, in vec3 wb ) {
     return Clamp( (wa.x*wb.x + wa.y*wb.y) / sqrt( wa.x*wa.x + wa.y*wa.y ) * ( wb.x*wb.x + wb.y*wb.y ),
 	               -1.0f, 1.0f );
 }
 
-pure vec3 Reflect( in vec3 wo, in vec3 n ) {
+pure @safe @nogc nothrow
+vec3 Reflect( in vec3 wo, in vec3 n ) {
     return -1.0f*wo + 2.0f*v_dot( wo, n )*n;
 }
 
-pure bool
-Refract( in vec3 wi, in vec3 n, float eta, ref vec3 o_wt )
+pure @safe @nogc nothrow
+bool Refract( in vec3 wi, in vec3 n, float eta, ref vec3 o_wt )
 {
     float cosThetaI = v_dot( n, wi );
 	float sin2ThetaI = Max( 0.0f, 1.0f - cosThetaI * cosThetaI );
@@ -101,11 +107,13 @@ Refract( in vec3 wi, in vec3 n, float eta, ref vec3 o_wt )
 }
 
 
-pure bool SameHemisphere( in vec3 a, in vec3 b ) {
+pure @safe @nogc nothrow
+bool SameHemisphere( in vec3 a, in vec3 b ) {
     return ( a.z * b.z ) > 0.0f;
 }
 
-pure vec3 FaceForward( in vec3 v, in vec3 n ) {
+pure @safe @nogc nothrow
+vec3 FaceForward( in vec3 v, in vec3 n ) {
     return ( v_dot( v, n ) >= 0.0f ) ? n : -1.0f*n;
 }
 
@@ -117,13 +125,14 @@ abstract class BaseBxDF
 {
     const BxDFType    m_type;
 
+    pure @safe @nogc nothrow
 	this( BxDFType type )
 	{
 	    m_type = type;
 	}
 
-	pure const bool
-	MatchesType( BxDFType type ) {
+	pure const @safe @nogc nothrow
+	bool MatchesType( BxDFType type ) {
 	    return ( m_type & type ) == m_type;
 	}
 
@@ -141,15 +150,16 @@ abstract class BaseBxDF
         Returns:
             The ratio of reflected irradiance heading towards wo
     */
-	pure const vec3 F( in vec3 wo, in vec3 wi );
+	pure const @safe @nogc nothrow
+	vec3 F( in vec3 wo, in vec3 wi );
 
     // F_TODO:: Document the parameters of these methods.
 	
     /**
         Generates a sampling direction (o_wi) and PDF (o_pdf) give na random sample u (2D uniform random number)
     */
-	pure const vec3
-	Sample_F(
+	pure const @safe @nogc nothrow
+	vec3 Sample_F(
 	    in vec3     wo,    
 		in vec2     u,
 		ref vec3    o_wi,
@@ -159,84 +169,23 @@ abstract class BaseBxDF
 	/**
         Returns the PDF value for a pair of incoming and outgoing directions
     */
-	pure const float Pdf( in vec3 wo, in vec3 wi );
+	pure const @safe @nogc nothrow
+	float Pdf( in vec3 wo, in vec3 wi );
 
 
 	/**
         Compute the Hemispherical-Directional reflectance on the surface in the direction wo
         The integral of the BDRF toward wo over the hemisphere of incoming directions
     */
-	pure const vec3 Rho( in vec3 wo, in vec2[] samples );
+	pure const @safe @nogc nothrow
+	vec3 Rho( in vec3 wo, in vec2[] samples );
 
 	/**
         Compute the average Hemispherical-Hemispherical reflection on the surface.
         The same as the Hemispheriecal-Directional reflectance, but averaged over the hemisphere
           of outgoing directions.
     */
-	pure const vec3 Rho( in vec2[] samples1, in vec2[] samples2 );
+	pure const @safe @nogc nothrow
+	vec3 Rho( in vec2[] samples1, in vec2[] samples2 );
 }
 
-
-
-struct BSDF
-{
-    vec3  m_geoNormal;
-
-	//  Can be used to transform in/out of the shading coordinate system
-	//  { m_shadingNormal, m_shadingS, m_shadingT } form an orthonormal basis on the surface of the shading point
-	//
-	vec3  m_shadingNormal;
-	vec3  m_shadingS;
-	vec3  m_shadingT;
-
-	BaseBxDF*[8]  m_bxdfs;
-    uint          m_numBxdfs = 0;
-	
-	float         m_eta; // Relative index of refraction
-
-	this( in vec3 geoNormal, in vec3 shadingNormal, in vec3 dpdu, float eta = 1.0f )
-	{
-	    m_geoNormal     = geoNormal;
-		m_shadingNormal = shadingNormal;
-		m_shadingS      = v_normalise( dpdu );
-		m_shadingT      = v_normalise( v_cross( shadingNormal, dpdu ) );
-		m_eta           = eta;
-	}
-
-	void AddBxDF( BaseBxDF* bxdf ) {
-	    assert ( m_numBxdfs <= m_bxdfs.length, "Trying to insert to many BxDFs into BSDF object" );
-
-		m_bxdfs[ m_numBxdfs ] = bxdf;
-		++m_numBxdfs;
-	}
-
-	pure const uint
-	NumComponents( BxDFType flags = BxDFType.All )
-	{
-	    uint numMatching = 0;
-	    foreach (i; 0..m_numBxdfs ) {
-		    if ( m_bxdfs[i].MatchesType( flags ) )
-			    ++numMatching;
-		}
-		return numMatching;
-	}
-
-	pure const vec3
-	WorldToLocal( in vec3 w )
-	{
-	    return vec3(
-		    v_dot( w, m_shadingS ),
-			v_dot( w, m_shadingT ),
-			v_dot( w, m_shadingNormal ) );
-	}
-
-	pure const vec3
-	LocalToWorld( in vec3 w )
-	{
-	    return vec3(
-		    m_shadingS.x*w.x + m_shadingT.x*w.y + m_shadingNormal.x*w.z,
-		    m_shadingS.y*w.x + m_shadingT.y*w.y + m_shadingNormal.y*w.z,
-		    m_shadingS.z*w.x + m_shadingT.z*w.y + m_shadingNormal.z*w.z,
-		);
-	}
-}
